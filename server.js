6e2bc30fe0692b0272dfb190442b8f870d9e0ca9
P Bridwell <patbridwell@cox.net>
2020-06-15 10:28:59 -0500

    starts express server. Brings in required modules

---
+++ server.js
@@ -0,0 +1,32 @@
+// Bringing in dependencies
+require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan")
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+// Handle data-parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+//==================================================================
+// Mongoose connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/passworddb", { useNewUrlParser: true });
+// Set handlebars
+// Set Handlebars.
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require('./routes/htmlRoutes.js'));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
